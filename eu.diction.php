e59aef7a8a43e3aada9eb71d2fb24ecb9b283caa
Karl August Nestor <jesuscaesar@outlook.com>
2023-01-05 01:38:00 +0000

    Added new wallpapers

--- eu.diction.php
+++ eu.diction.php
@@ -71,6 +71,12 @@ $diction['eu'] =
         '400' => 'cannot reach',
         '500' => 'is dead'
     ],
+    'trade' =>
+    [
+        'quid' => 'QUID',
+        'pro' => 'PRO',
+        'quo' => 'QUO'
+    ],
     'transaction' =>
     [
         '00' => 'TRANSACTION_APPROVED',
